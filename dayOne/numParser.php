f8c91c64c4c26f223ae07f7d52dd635c07b9811c
Jeroen de Klerk <jeroen.de.klerk@basgroup.com>
2023-12-07 11:15:59 +0100

    Init and day one solution

---
+++ dayOne/numParser.php
@@ -0,0 +1,16 @@
+<?php
+
+$coords = file('./input.txt', FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
+$numbers = [];
+
+foreach ($coords as $s) {
+    $intOnly = array_filter(mb_str_split($s), function ($c) {
+        return ctype_digit($c);
+    });
+
+    $numToAdd = reset($intOnly) . end($intOnly);
+    $numbers[] = (int)$numToAdd;
+    echo "String $s contains number $numToAdd\n";
+}
+
+var_export(array_sum($numbers));
